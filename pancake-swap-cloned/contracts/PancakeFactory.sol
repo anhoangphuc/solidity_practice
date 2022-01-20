72b77c7cbccadbb970d54b2da9030f44a0e6b786
Sotatek-PhucTa <hoangphucnb97@gmail.com>
2022-01-20 18:12:25 +0700

    Add PancakeFactory

---
+++ pancake-swap-cloned/contracts/PancakeFactory.sol
@@ -0,0 +1,413 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity 0.8.11;
+
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/math/Math.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IPancakeFactory {
+    event PairCreated(address indexed token0, address indexed token1, address pair, uint);
+
+    function feeTo() external view returns (address);
+    function feeToSetter() external view returns (address);
+
+    function getPair(address tokenA, address tokenB) external view returns (address pair);
+    function allPairs(uint) external view returns (address pair);
+    function allPairsLength() external view returns (uint);
+
+    function createPair(address tokenA, address tokenB) external returns (address pair);
+
+    function setFeeTo(address) external;
+    function setFeeToSetter(address) external;
+}
+
+interface IPancakePair {
+    event Approval(address indexed owner, address indexed spender, uint value);
+    event Transfer(address indexed from, address indexed to, uint value);
+
+    function name() external pure returns (string memory);
+    function symbol() external pure returns (string memory);
+    function decimals() external pure returns (uint8);
+    function totalSupply() external view returns (uint8);
+    function balanceOf(address owner) external view returns (uint);
+    function allowance(address owner, address spender) external returns (uint);
+
+    function approve(address spender, uint value) external returns (bool);
+    function transfer(address to, uint value) external returns (bool);
+    function transferFrom(address from, address to, uint value) external returns (bool);
+
+    function DOMAIN_SEPERATOR() external view returns (bytes32);
+    function PERMIT_TYPEHASH() external pure returns (bytes32);
+    function nonces(address owners) external view returns (uint);
+
+    function permit(address owner, address spender, uint value, uint deadline, uint8 v, bytes32 r, bytes32 s) external;
+
+    event Mint(address indexed sender, uint amount0, uint amount1);
+    event Burn(address indexed sender, uint amount0, uint amount1, address indexed to);
+    event Swap(
+        address indexed sender,
+        uint amount0In,
+        uint amount1In,
+        uint amount0Out,
+        uint amount1Out,
+        address indexed to
+    );
+    event Sync(uint112 reverse0, uint112 reverse1);
+
+    function MINIMUM_LIQUIDITY() external pure returns (uint);
+    function factory() external view returns (address);
+    function token0() external view returns (address);
+    function token1() external view returns (address);
+    function getReverses() external view returns (uint112 reverse0, uint112 reverse1, uint32 blockTimestampLast);
+    function price0CummulativeLast() external view returns (uint);
+    function price1CummulativeLast() external view returns (uint);
+    function kLast() external view returns (uint);
+
+    function mint(address to) external returns (uint liquidity);
+    function burn(address to) external returns (uint amount0, uint amount1);
+    function swap(uint amount0Out, uint amount1Out, address to, bytes calldata data) external;
+    function skim(address to) external;
+    function sync() external;
+
+    function initialize(address, address) external;
+}
+
+
+interface IPancakeERC20 {
+    event Approval(address indexed owner, address indexed spender, uint value);
+    event Transfer(address indexed from, address indexed to, uint value);
+
+    function name() external pure returns (string memory);
+    function symbol() external pure returns (string memory);
+    function decimals() external pure returns (uint8);
+    function totalSupply() external view returns (uint);
+    function balanceOf(address owner) external view returns (uint);
+    function allowance(address owner, address spender) external view returns (uint);
+
+        function approve(address spender, uint value) external returns (bool);
+    function transfer(address to, uint value) external returns (bool);
+    function transferFrom(address from, address to, uint value) external returns (bool);
+
+    function DOMAIN_SEPARATOR() external view returns (bytes32);
+    function PERMIT_TYPEHASH() external pure returns (bytes32);
+    function nonces(address owner) external view returns (uint);
+
+    function permit(address owner, address spender, uint value, uint deadline, uint8 v, bytes32 r, bytes32 s) external;
+}
+
+contract PancakeERC20 is IPancakeERC20 {
+    using SafeMath for uint;
+
+    string public constant name = 'Pancake LPs';
+    string public constant symbol = 'Cake-LP';
+    uint8 public constant decimals = 18;
+    uint public totalSupply;
+
+    mapping(address => uint) public balanceOf;
+    mapping(address => mapping(address => uint)) public allowance;
+
+    bytes32 public DOMAIN_SEPARATOR;
+    // keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
+    bytes32 public constant PERMIT_TYPEHASH = 0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
+    mapping(address => uint) public nonces;
+
+    constructor() {
+        uint chainId;
+        assembly { chainId := chainId }
+        DOMAIN_SEPARATOR = keccak256(
+            abi.encode(
+                keccak256('EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)'),
+                keccak256(bytes(name)),
+                keccak256(bytes('1')),
+                chainId,
+                address(this)
+            )
+        );
+    }
+
+    function _mint(address to, uint value) internal {
+        totalSupply = totalSupply.add(value);
+        balanceOf[to] = balanceOf[to].add(value);
+        emit Transfer(address(0), to, value);
+    }
+
+    function _burn(address from, uint value) internal {
+        balanceOf[from] = balanceOf[from].sub(value);
+        totalSupply = totalSupply.sub(value);
+        emit Transfer(from, address(0), value);
+    }
+
+    function _approve(address owner, address spender, uint value) private {
+        allowance[owner][spender] = value;
+        emit Approval(owner, spender, value);
+    }
+
+    function _transfer(address from, address to, uint value) private {
+        balanceOf[from] = balanceOf[from].sub(value);
+        balanceOf[to] = balanceOf[to].add(value);
+        emit Transfer(from, to, value);
+    }
+
+    function approve(address spender, uint value) external returns (bool) {
+        _approve(msg.sender, spender, value);
+        return true;
+    }
+
+    function transfer(address to, uint value) external returns (bool) {
+        _transfer(msg.sender, to, value);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint value) external returns (bool) {
+        allowance[from][msg.sender] = allowance[from][msg.sender].sub(value);
+        _transfer(from, to, value);
+        return true;
+    }
+
+    function permit(address owner, address spender, uint value, uint deadline, uint8 v, bytes32 r, bytes32 s) external {
+        require(deadline >= block.timestamp, 'Pancake: Expired');
+        bytes32 digest = keccak256(
+            abi.encodePacked('\x19\x01', DOMAIN_SEPARATOR, keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, value, nonces[owner]++, deadline)))
+        );
+        address recoveredAddress = ecrecover(digest, v, r, s);
+        require(recoveredAddress != address(0) && recoveredAddress == owner, 'Pancake: INVALID_SIGNATURE');
+        _approve(owner, spender, value);
+    }
+}
+
+library UQ112x112 {
+    uint224 constant Q112 = 2**112;
+
+    // encode a uint112 as a UQ112x112
+    function encode(uint112 y) internal pure returns (uint224 z) {
+        z = uint224(y) * Q112; // never overflows
+    }
+
+    // divide a UQ112x112 by a uint112, returning a UQ112x112
+    function uqdiv(uint224 x, uint112 y) internal pure returns (uint224 z) {
+        z = x / uint224(y);
+    }
+}
+
+interface IPancakeCallee {
+    function pancakeCall(address sender, uint amount0, uint amount1, bytes calldata data) external;
+}
+
+contract PancakePair is PancakeERC20, IPancakePair {
+    using SafeMath for uint;
+    using SafeMath for uint112;
+    using UQ112x112 for uint224;
+
+    uint public constant MINIMUM_LIQUIDITY = 10 ** 3;
+    bytes4 private constant SELECTOR = bytes4(keccak256(bytes('transfer(address,uint256)')));
+
+    address public factory;
+    address public token0;
+    address public token1;
+
+    uint112 private reverse0;
+    uint112 private reverse1;
+    uint32 private blockTimestampLast;
+
+    uint public price0CummulativeLast;
+    uint public price1CummulativeLast;
+    uint public kLast;
+
+    uint private unlocked = 1;
+    modifier lock() {
+        require(unlocked == 1, 'Pancake: LOCKED');
+        unlocked = 0;
+        _;
+        unlocked = 1;
+    }
+
+    function getReverses() public view returns (uint112 _reverse0, uint112 _reverse1, uint32 _blockTimestampLast) {
+        _reverse0 = reverse0;
+        _reverse1 = reverse1;
+        _blockTimestampLast = blockTimestampLast;
+    }
+
+    function _safeTransfer(address token, address to, uint value) private {
+        (bool success, bytes memory data) = token.call(abi.encodeWithSignature('transfer(address,uint256)', to, value));
+        require(success && (data.length == 0 || abi.decode(data, (bool))), 'Pancake: TRANSFER_FAILED');
+    }
+
+    constructor() {
+        factory = msg.sender;
+    }
+
+    function initialize(address _token0, address _token1) external {
+        require(msg.sender == factory, 'PANCAKE: FORBIDDEN');
+        token0 = _token0;
+        token1 = _token1;
+    }
+
+    function _update(uint balance0, uint balance1, uint112 _reverse0, uint112 _reverse1) private {
+        uint32 blockTimestamp = uint32(block.timestamp % 2 ** 32);
+        uint32 timeElapsed = blockTimestamp - blockTimestampLast;
+        if (timeElapsed > 0 && _reverse0 != 0 && reverse1 != 0) {
+            price0CummulativeLast = uint(UQ112x112.encode(_reverse1).uqdiv(_reverse0)) * timeElapsed;
+            price1CummulativeLast = uint(UQ112x112.encode(_reverse0).uqdiv(_reverse1)) * timeElapsed;
+        }
+
+        reverse0 = uint112(balance0);
+        reverse1 = uint112(balance1);
+        blockTimestampLast = blockTimestamp;
+        emit Sync(reverse0, reverse1);
+    }
+
+    function _mintFee(uint112 _reverse0, uint112 _reverse1) private returns (bool feeOn) {
+        address feeTo = IPancakeFactory(factory).feeTo();
+        feeOn = feeTo != address(0);
+        uint _kLast = kLast;
+        if (feeOn) {
+            if (_kLast != 0) {
+                uint rootK = Math.sqrt(uint(_reverse0.mul(_reverse1)));
+                uint rootKLast = Math.sqrt(_kLast);
+                if (rootK > rootKLast) {
+                    uint numerator = totalSupply.mul(rootK.sub(rootKLast)).mul(8);
+                    uint denominator = rootK.mul(17).add(rootKLast.mul(8));
+                    uint liquidity = numerator / denominator;
+                    if (liquidity > 0) _mint(feeTo, liquidity);
+                }
+            }
+        } else if (_kLast != 0) {
+            kLast = 0;
+        }
+    }
+
+    function mint(address to) external lock returns (uint liquidity) {
+        (uint112 _reverse0, uint112 _reverse1,) = getReverses();
+        uint balance0 = IERC20(token0).balanceOf(address(this));
+        uint balance1 = IERC20(token1).balanceOf(address(this));
+        uint amount0 = balance0.sub(_reverse0);
+        uint amount1 = balance1.sub(_reverse1);
+
+        bool feeOn = _mintFee(_reverse0, _reverse1);
+        uint _totalSupply = totalSupply;
+        if (_totalSupply == 0) {
+            liquidity = Math.sqrt(amount0.mul(amount1).sub(MINIMUM_LIQUIDITY));
+            _mint(address(0), MINIMUM_LIQUIDITY);
+        } else {
+            liquidity = Math.min(amount0.mul(_totalSupply) / _reverse0, amount1.mul(_totalSupply) / _reverse1);
+        }
+        require(liquidity > 0, 'Pancake: INSUFFICIENT_LIQUIDITY_MINTED');
+        _mint(to, liquidity);
+
+        _update(balance0, balance1, _reverse0, _reverse1);
+        if (feeOn) kLast = uint(reverse0.mul(reverse1));
+        emit Mint(msg.sender, amount0, amount1);
+    }
+
+    function burn(address to) external lock returns (uint amount0, uint amount1) {
+        (uint112 _reverse0, uint112 _reverse1,) = getReverses();
+        address _token0 = token0;
+        address _token1 = token1;
+        uint balance0 = IERC20(_token0).balanceOf(address(this));
+        uint balance1 = IERC20(_token1).balanceOf(address(this));
+        uint liquidity = balanceOf[address(this)];
+
+        bool feeOn = _mintFee(_reverse0, _reverse1);
+        uint _totalSupply = totalSupply;
+        amount0 = liquidity.mul(balance0) / _totalSupply;
+        amount1 = liquidity.mul(balance1) / _totalSupply;
+        require(amount0 > 0 && amount1 > 0, 'Pancake: INSUFFICIENT_LIQUIDITY_BURNED');
+        _burn(address(this), liquidity);
+        _safeTransfer(_token0, to, amount0);
+        _safeTransfer(_token1, to, amount1);
+        balance0 = IERC20(_token0).balanceOf(address(this));
+        balance1 = IERC20(_token1).balanceOf(address(this));
+
+        _update(balance0, balance1, _reverse0, _reverse1);
+        if (feeOn) kLast = uint(reverse0).mul(reverse1);
+        emit Burn(msg.sender, amount0, amount1, to);
+    }
+
+    function swap(uint amount0Out, uint amount1Out, address to, bytes calldata data) external lock {
+        require(amount0Out > 0 || amount1Out > 0, 'Pancake: INSUFFICIENT_OUTPUT_AMOUNT');
+        (uint112 _reverse0, uint112 _reverse1,) = getReverses();
+        require(amount0Out < _reverse0 && amount1Out < _reverse1, 'Pancake: INSUFFICIENT_LIQUIDITY');
+
+        uint balance0;
+        uint balance1;
+
+        {
+            address _token0 = token0;
+            address _token1 = token1;
+            require(to != _token0 && to != _token1, 'Pancake: INVALID_TO');
+            if (amount0Out > 0) _safeTransfer(_token0, to, amount0Out);
+            if (amount1Out > 0) _safeTransfer(_token1, to, amount1Out);
+            if (data.length > 0) IPancakeCallee(to).pancakeCall(msg.sender, amount0Out, amount1Out, data);
+            balance0 = IERC20(_token0).balanceOf(address(this));
+            balance1 = IERC20(_token1).balanceOf(address(this));
+        }
+
+        uint amount0In = balance0 > _reverse0 - amount0Out ? balance0 - (_reverse0 - amount0Out) : 0;
+        uint amount1In = balance1 > _reverse1 - amount1Out ? balance1 - (_reverse1 - amount1Out) : 0;
+        require(amount0In > 0 || amount1In > 0, 'Pancake: INSUFFICIENT_INPUT_AMOUNT');
+        {
+            uint balance0Adjusted = (balance0.mul(100000).sub(amount0In.mul(25)));
+            uint balance1Adjusted = (balance1.mul(100000).sub(amount1In.mul(25)));
+            require(balance0Adjusted.mul(balance1Adjusted) >= uint(_reverse0).mul(_reverse1).mul(100000 ** 2), 'Pancake: K');
+        }
+
+        _update(balance0, balance1, _reverse0, _reverse1);
+        emit Swap(msg.sender, amount0In, amount1In, amount0Out, amount1Out, to);
+    }
+
+    function skim(address to) external lock {
+        address _token0 = token0;
+        address _token1 = token1;
+        _safeTransfer(_token0, to, IERC20(_token0).balanceOf(address(this)).sub(reverse0));
+        _safeTransfer(_token1, to, IERC20(_token1).balanceOf(address(this)).sub(reverse1));
+    }
+
+    function sync() external lock {
+        _update(IERC20(token0).balanceOf(address(this)), IERC20(token1).balanceOf(address(this)), reverse0, reverse1);
+    }
+}
+
+contract PancakeFactory is IPancakeFactory {
+    bytes32 public constant INIT_CODE_PAIR_HASH = keccak256(abi.encodePacked(type(PancakePair).creationCode));
+
+    address public feeTo;
+    address public feeToSetter;
+
+    mapping(address => mapping(address => address)) public getPair;
+    address[] public allPairs;
+
+    constructor(address _feeToSetter) {
+        feeToSetter = _feeToSetter;
+    }
+
+    function allPairsLength() external view returns (uint) {
+        return allPairs.length;
+    }
+
+    function createPair(address tokenA, address tokenB) external view returns (address pair) {
+        require(tokenA != tokenB, 'Pancake: IDENTICAL_ADDRESSES');
+        (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        require(token0 != address(0), 'Pancake: ZERO_ADDRESSES');
+        require(getPair[token0][token1] == address(0), 'Pancake: PAIR_EXISTS');
+        bytes memory bytecode = type(PancakePair).creationCode;
+        bytes32 salt = keccak256(abi.encodePacked(token0, token1));
+        assembly {
+            pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
+        }
+        IPancakePair(pair).initialize(token0, token1);
+        getPair[token0][token1] = pair;
+        getPair[token1][token0] = pair;
+        allPairs.push(pair);
+        emit PairCreated(token0, token1, pair, allPairs.length);
+    }
+
+    function setFeeTo(address _feeTo) external {
+        require(msg.sender == feeToSetter, 'Pancake: FORBIDDEN');
+        feeTo = _feeTo;
+    }
+    
+    function setFeeToSetter(address _feeToSetter) external {
+        require(msg.sender == feeToSetter, 'Pancake: FORBIDDEN');
+        feeToSetter = _feeToSetter;
+    }
+}
